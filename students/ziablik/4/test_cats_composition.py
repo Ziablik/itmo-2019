bba1c3b678244d9e69c9b92ef78667e3e8645052
ziablik <ziablikl@mail.ru>
2020-04-06 00:27:33 +0300

    add test_cats_composition.py

---
+++ students/ziablik/4/test_cats_composition.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess  # noqa S404
+import unittest
+
+import pytest
+
+from cats_composition import CatProcessor, main
+from cats_direct import fetch_cat_fact, fetch_cat_image, save_cat
+
+
+class CatsCompositionTests(unittest.TestCase):
+    """Cats_composition tests class."""
+
+    @pytest.mark.remote_data
+    def test_main(self):
+        """Main function test."""
+        cat_processor = CatProcessor(
+            fetch_cat_fact,
+            fetch_cat_image,
+            save_cat,
+        )
+        main(
+            1,
+            process_cat=cat_processor,
+            show_information=print,  # noqa: T002
+        )
+        assert os.path.isfile('temp/cat_1_fact.txt')
+        assert os.path.isfile('temp/cat_1_image.jpg')
+        assert os.stat('temp/cat_1_fact.txt').st_size != 0
+
+    def test_integration(self):
+        """Test func."""
+        format_str = 'python students/ziablik/4/cats_composition.py {0}'  # noqa E800
+        command_str = format_str.format('--count=1')
+        assert subprocess.call(command_str, shell=True) == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
