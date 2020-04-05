6aee6f47d00163213e313611230d8a0374fcb081
ziablik <ziablikl@mail.ru>
2020-04-06 02:44:48 +0300

    fix for flake8

--- students/ziablik/6/pizzapp/pizzapp/wsgi.py
+++ students/ziablik/6/pizzapp/pizzapp/wsgi.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 """
 WSGI config for pizzapp project.
+
 It exposes the WSGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
