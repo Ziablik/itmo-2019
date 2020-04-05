e6a2c419dabdbb5b27cf6811a7a8d6fe8ea5af2d
ziablik <ziablikl@mail.ru>
2020-04-06 02:20:01 +0300

    upd get_statistics.py and post_order.py

--- students/ziablik/6/pizzapp/ziabpizza/usecases/get_statistics.py
+++ students/ziablik/6/pizzapp/ziabpizza/usecases/get_statistics.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
 class GetStatistics(object):
     """
-       Gets statistics on :term:`Pizza` ordered today.
-       .. literalinclude:: user_stories/get_statistics.feature
-         :language: gherkin
-       .. versionadded:: 0.1.0
+    Gets statistics on :term:`Pizza` ordered today.
+    .. literalinclude:: user_stories/get_statistics.feature
+      :language: gherkin
+    .. versionadded:: 0.1.0
     """
 
     def __call__(self):
-        pass
+        """Method to call when a user wants to see statistics."""
+        pass  # noqa: WPS420
--- students/ziablik/6/pizzapp/ziabpizza/usecases/post_order.py
+++ students/ziablik/6/pizzapp/ziabpizza/usecases/post_order.py
@@ -1,10 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from ziabpizza.logic.cooking import count_cooking_time
+from ziabpizza.logic.mail import send_mail_on_order
+from ziabpizza.models import Order
+from ziabpizza.serializers import OrderSerializer
+
+
 class PostOrder(object):
     """
-       Posts new :term:`Order`.
-       .. literalinclude:: user_stories/post_order.feature
-         :language: gherkin
-       .. versionadded:: 0.1.0
+    Posts new :term:`Order`.
+    .. literalinclude:: user_stories/post_order.feature
+      :language: gherkin
+    .. versionadded:: 0.1.0
     """
 
-    def __call__(self):
-        pass
+    def __call__(self, incoming_data):
+        """Method to call when a user wants to put an :term:`Order`."""
+        serializer = OrderSerializer(data=incoming_data)
+        if serializer.is_valid():
+            serializer.save()
+            order = Order.objects.get(pk=serializer.data['id'])
+            order.cooking_time = count_cooking_time(order.creation_date)
+            order.save()
+            send_mail_on_order(order)
+        return serializer.data, serializer.errors
