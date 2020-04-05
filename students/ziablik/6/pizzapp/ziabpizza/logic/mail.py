d69e4a8b1b07414fd71c97865c9f61885d74d2a4
ziablik <ziablikl@mail.ru>
2020-04-06 02:18:46 +0300

    create logic cooking.py and mail.py

---
+++ students/ziablik/6/pizzapp/ziabpizza/logic/mail.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from django.core.mail import send_mail
+
+from ziabpizza.logic.cooking import count_cooking_time
+from ziabpizza.models import Order
+
+
+def send_mail_on_order(order: Order):
+    """Sends mail concerning any order."""
+    send_mail(
+        'Your pizza is on the way!',
+        'It will arrive in {0}'.format(count_cooking_time(order.creation_date)),
+        'ziablikl@mail.ru',
+        [order.client_email],
+        fail_silently=False,
+    )
+    order.email_is_sent = True
+    order.save()
+    return order
