d344fc4e253f8f6b051dde8cdb52d6496c2a31ac
ziablik <ziablikl@mail.ru>
2020-04-06 05:06:21 +0300

    add conftest.py for start migrations on ci

---
+++ students/ziablik/6/pizzapp/ziabpizza/conftest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess  # noqa: S404
+
+import django
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzapp.settings')
+
+
+def pytest_configure():
+    settings.DEBUG = False
+    django.setup()
+
+    cmd1 = 'python students/ziablik/6/pizzapp/manage.py makemigrations'
+    cmd2 = 'python students/ziablik/6/pizzapp/manage.py migrate'
+
+    subprocess.check_call(cmd1, shell=True)  # noqa: S607, S602
+    subprocess.check_call(cmd2, shell=True)  # noqa: S607, S602
