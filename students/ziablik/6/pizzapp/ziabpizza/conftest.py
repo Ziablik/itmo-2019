9addcfa83ffa7282cfba7910b65b66732d049609
ziablik <ziablikl@mail.ru>
2020-04-06 05:09:01 +0300

    fix flake8 in conftest.py

--- students/ziablik/6/pizzapp/ziabpizza/conftest.py
+++ students/ziablik/6/pizzapp/ziabpizza/conftest.py
@@ -10,6 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzapp.settings')
 
 
 def pytest_configure():
+    """`Pytest` automatically calls this function once when tests are run."""
     settings.DEBUG = False
     django.setup()
 
