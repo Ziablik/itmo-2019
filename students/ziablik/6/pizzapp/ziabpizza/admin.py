b0ce64467e57e33036475a0b8b3ff5102ef6e142
ziablik <ziablikl@mail.ru>
2020-04-06 18:50:41 +0300

    delete admin.py

--- students/ziablik/6/pizzapp/ziabpizza/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Ingredient, Order, Pizza   # noqa: I001
-
-admin.site.register(Ingredient)
-admin.site.register(Order)
-admin.site.register(Pizza)
